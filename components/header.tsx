599a6620e5e84e413340ce8be42280931e82ebee
Avisa <avisafeizi1383@gmail.com>
2025-12-27 17:38:37 +0330

    completed header

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,7 @@
 
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
-import { LayoutDashboard, Link } from 'lucide-react';
+import { LayoutDashboard, Link, PenBox } from 'lucide-react';
 import Image from 'next/image';
 import { Button } from './ui/button';
 import { Sign } from 'crypto';
@@ -15,13 +15,18 @@ const Header = () => {
           className='h-12 w-auto object-contain' priority/>
         </Link>
         
-        <div>
+        <div className='flex item-center space-x-4'>
           <SignedIn>
-            <Link href={"/dashboard"}>
-             
+            <Link href={"/dashboard"} className='text-gray-600 hover:text-blue-600 flex items-center gap-2'> 
               <Button  variant={'outline'}>
                 <LayoutDashboard size={18}/>
-                <span> Dashboard</span>
+                <span className="hidden md:inline"> Dashboard</span>
+              </Button>
+            </Link>
+            <Link href={"/transaction/create"}> 
+              <Button  className='flex items-center gap-2'> 
+                <PenBox size={18}/>
+                <span className="hidden md:inline"> Add Transaction</span>
               </Button>
             </Link>
           </SignedIn>
@@ -35,13 +40,12 @@ const Header = () => {
 
             </SignedOut>
             <SignedIn>
-              <UserButton />
+              <UserButton appearance={{elements: {
+                avatarBox:'w-10 h-10',
+              },}}/>
             </SignedIn>
         </div> 
-         </nav>
-          
-
-          
+         </nav>          
     </div>
   )
 }
